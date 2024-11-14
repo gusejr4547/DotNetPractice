08216126d2db57f017bc69ba6803431e183ecf1c
조현덕 <gusejr4547@gmail.com>
2024-11-14 15:57:54 +0900

    feat : Todo 엔티티 추가

---
+++ WebApplicationPractice/Todo/Entity/Todo.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplicationPractice.Todo.Entity
+{
+    public class Todo
+    {
+        public long Id{ get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public Todo(string name, bool isComplete = false) { 
+            this.Name = name;
+            this.IsComplete = isComplete;
+        }
+
+    }
+}
---
+++ WebApplicationPractice/Todo/Entity/TodoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationPractice.Todo.Entity
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; } = null!;
+    }
+}
